c1fc3a913b120fbad10ec114645f0ec99aa08e59
Imad Bousbaa <bousbaa.imadeddine@gmail.com>
2018-03-29 16:54:32 +0100

    Add documentation

--- calcul.py
+++ calcul.py
@@ -1,5 +1,17 @@
-print('hello')
+class Triangle(object):
+    def __init__(self, *args):
+        super(Triangle, self).__init__(*args))
+        
 def findmatch(n):
+    """Find the bell numbers in the Stirling triangle of the second kind.
+    
+    Arguments:
+        n {int} -- The top range of the Bell number B_n.
+    
+    Returns:
+        list -- list of matchs.
+    """
+
     res=[]
     for i in range(2,n):
         bell=bell_number(i)
